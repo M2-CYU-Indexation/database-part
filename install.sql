acff42a41f92c33b26104dc6630af14777a83bb6
NicolasCIBULKA <nico.cibulka@gmail.com>
2022-09-20 11:31:15 +0200

    init all lines with empty values

--- install.sql
+++ install.sql
@@ -27,43 +27,62 @@ create table imageTable(
     isRGB number(1,0)
 );
 
-/*
+
 declare
     i ordsys.ordimage;
+    imgName varchar2(50);
     ctx RAW(400) := NULL;
-    ligne multimedia%ROWTYPE;
-    cursor mm is
-    select * from imageTable
-    for update;
+    ligne imageTable%ROWTYPE;
 begin
+-- insertion des images vides
 for imageName in 1..500
 loop
-    name = concat(imageName, '.jpg')
-end loop
-
-
-
-
--- insertion d’une image, de contenu vide
-insert into multimedia(nom, image, signature)
-values (’image1.jpg’, ordsys.ordimage.init(), ordsys.ordimageSignature.init());
+    imgName := concat(imageName, '.jpg');
+    insert into imageTable(imageName, image, signature, width, height, grayHistogram, redHistogram, greenHistogram, blueHistogram, redRatio, greenRatio, blueRatio, averageColor, gradientNormMean, outlinesMinX , outlinesMinY,outlinesMaxX,outlinesMaxY,outlinesBarycenterX,outlinesBarycenterY ,nbOutlinePixel ,isRGB)
+values (imgName, ordsys.ordimage.init(), ordsys.ordimageSignature.init(), null,null, null, null, null, null, null, null, null, null, null, null,null,null,null,null,null,null,null );
 commit;
--- chargement du contenu de l’image a partir du fichier
-select image into i
-from multimedia
-where nom = ’image1.jpg’
-for update;
-i.importFrom(ctx, ’file’, ’IMG’, ’image1.jpg’);
-update multimedia
-set image = i
-where nom = ’image1.jpg’;
-commit;
--- proceder de meme pour les autres images
--- generation des signatures
-for ligne in mm loop
-ligne.signature.generateSignature(ligne.image);
-update multimedia
-set signature = ligne.signature
-where current of mm;
-endloop;
-commit;*/
+end loop;
+end;
+
+--declare
+    --i ordsys.ordimage;
+    --name string;
+    --ctx RAW(400) := NULL;
+    --ligne multimedia%ROWTYPE;
+    --cursor mm is
+    --select * from imageTable
+    --for update;
+--begin
+---- insertion des images vides
+--for imageName in 1..500
+--loop
+    --name = concat(imageName, '.jpg')
+    --insert into multimedia(nom, image, signature)
+--values (name, ordsys.ordimage.init(), ordsys.ordimageSignature.init());
+--commit;
+--end loop
+
+
+
+
+---- insertion d’une image, de contenu vide
+
+---- chargement du contenu de l’image a partir du fichier
+--select image into i
+--from multimedia
+--where nom = ’image1.jpg’
+--for update;
+--i.importFrom(ctx, ’file’, ’IMG’, ’image1.jpg’);
+--update multimedia
+--set image = i
+--where nom = ’image1.jpg’;
+--commit;
+---- proceder de meme pour les autres images
+---- generation des signatures
+--for ligne in mm loop
+--ligne.signature.generateSignature(ligne.image);
+--update multimedia
+--set signature = ligne.signature
+--where current of mm;
+--endloop;
+--commit;
